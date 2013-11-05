6f8bd2ea646eecdd738a8f56d2beaf09c882a44a
Laurent Pouilloux <lolo@pouilloux.org>
2013-11-05 16:02:42 +0100

    fix bandwith => bandwidth, Change default routing from Full to Floyd, remove minidom dependancy

--- G5K_simgrid.py
+++ G5K_simgrid.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import os, json
 import xml.etree.ElementTree as ET
-import xml.dom.minidom
 from pprint import pprint
 from execo import logger
 from execo_g5k import api_utils as API
@@ -15,7 +14,7 @@ from itertools import product
 logger.setLevel('INFO')
 
 suffix = ''
-default_routing = 'Full'
+default_routing = 'Floyd'
 latency = 2.25E-3
 
 sites = sorted(API.get_g5k_sites())
@@ -115,7 +114,7 @@ for ne in data_backbone:
                 
                 if 'bordeaux' not in dst:
                     if not gr.has_node(dst): gr.add_node(dst, kind = kind)
-                    if not gr.has_edge(src, dst): gr.add_edge(src, dst, bandwith = rate, latency = latency)
+                    if not gr.has_edge(src, dst): gr.add_edge(src, dst, bandwidth = rate, latency = latency)
 # Adding sites equipements, links and hosts to the graphs
 site_gr = {}
 for site in sites:    
@@ -134,11 +133,11 @@ for site in sites:
                     if not sgr.has_node(dst): 
                         sgr.add_node(dst, kind = kind)
                     if not sgr.has_edge(src, dst): 
-                        sgr.add_edge(src, dst, bandwith = rate, latency = latency)
+                        sgr.add_edge(src, dst, bandwidth = rate, latency = latency)
                     else:
-                        tmp = nx.get_edge_attributes(sgr, 'bandwith')
+                        tmp = nx.get_edge_attributes(sgr, 'bandwidth')
                         if (src, dst) in tmp.keys():
-                            nx.set_edge_attributes(sgr, 'bandwith', {(src, dst): rate+tmp[(src, dst)]})                                
+                            nx.set_edge_attributes(sgr, 'bandwidth', {(src, dst): rate+tmp[(src, dst)]})                                
     for cluster, cl_hosts in data_hosts[site].iteritems():
         for host in cl_hosts:
             src = host['uid']+'.'+site
@@ -146,20 +145,21 @@ for site in sites:
                 sgr.add_node(src, kind = 'node', power = host['performance']['core_flops'], core = host['architecture']['smt_size'])        
             for adapt in filter(lambda n: n['enabled'] and not n['management'] and n['interface'] == 'Ethernet', host['network_adapters']):
                 dst = adapt['switch']+'.'+site
-                if not sgr.has_edge(src, dst): sgr.add_edge(src, dst, bandwith = adapt['rate'], latency = latency)
+                if not sgr.has_edge(src, dst): sgr.add_edge(src, dst, bandwidth = adapt['rate'], latency = latency)
     site_gr[site] = sgr          
                 
 
 # Creating the AS
-platform = ET.Element('platform')
+platform = ET.Element('platform', attrib = {'version': '3'})
 main_as = ET.SubElement(platform, 'AS', attrib = {'id': 'grid5000.fr', 'routing': default_routing})    
 for site in sites:
     ET.SubElement(main_as, 'AS', attrib = {'id': site+suffix, 'routing': default_routing})
 # Creating the backbone links
-for element1, element2, attrib in sorted(gr.edges_iter(data=True)):    
+for element1, element2, attrib in sorted(gr.edges_iter(data=True)):
+    element1, element2 = sorted( [element1, element2 ] )   
     ET.SubElement(main_as, 'link', attrib = {'id': element1+'_'+element2, 
                                               'latency': str(attrib['latency']), 
-                                              'bandwith': str(attrib['bandwith'])})
+                                              'bandwidth': str(attrib['bandwidth'])})
 # Creating the backbone routes between gateways
 gws = [ n for n,d in gr.nodes_iter(data=True) if 'gw' in n ]
 for el in product(gws, gws):
@@ -172,7 +172,8 @@ for el in product(gws, gws):
                         'dst': el[1].split('.')[0].split('-')[1]+suffix} )
             path = nx.shortest_path(gr, el[0], el[1])
             for i in range(len(path)-1):
-                ET.SubElement( asroute, 'link_ctn', attrib = { 'id': path[i]+'_'+path[i+1]} )
+                el1, el2 = sorted( [path[i], path[i+1] ] )
+                ET.SubElement( asroute, 'link_ctn', attrib = { 'id': el1+'_'+el2} )
 # Creating the elements on each site
 for site, sgr in site_gr.iteritems():
     site_el = main_as.find("./AS/[@id='"+site+"']")
@@ -191,21 +192,40 @@ for site, sgr in site_gr.iteritems():
         element1, element2 = sorted([element1, element2])
         ET.SubElement(site_el, 'link', attrib = {'id': element1+'_'+element2, 
                                               'latency': str(attrib['latency']), 
-                                              'bandwith': str(attrib['bandwith']) })
+                                              'bandwidth': str(attrib['bandwidth']) })
     for n,d in hosts:
         route = ET.SubElement(site_el, 'route', attrib = { 
                         'src': 'gw-'+site+'.'+site+suffix,
                         'dst': n+suffix} )
         path = nx.shortest_path(sgr, 'gw-'+site+'.'+site, n)
         for i in range(len(path)-1):
-            ET.SubElement( route, 'link_ctn', attrib = { 'id': path[i]+'_'+path[i+1]} )
-# Generating the XML file
+            el1, el2 = sorted( [path[i], path[i+1] ] )
+            ET.SubElement( route, 'link_ctn', attrib = { 'id': el1+'_'+el2} )
 
-platform = xml.dom.minidom.parseString("<?xml version='1.0' encoding='iso-8859-1'?>"+
-                                       '<!DOCTYPE platform SYSTEM "http://simgrid.gforge.inria.fr/simgrid.dtd">'+
-                                       ET.tostring(platform))
+# Generating the XML file
+def indent(elem, level=0):
+  i = "\n" + level*"  "
+  if len(elem):
+    if not elem.text or not elem.text.strip():
+      elem.text = i + "  "
+    if not elem.tail or not elem.tail.strip():
+      elem.tail = i
+    for elem in elem:
+      indent(elem, level+1)
+    if not elem.tail or not elem.tail.strip():
+      elem.tail = i
+  else:
+    if level and (not elem.tail or not elem.tail.strip()):
+      elem.tail = i
+
+indent(platform)
+tree = ET.ElementTree (platform)
 f = open('g5k_platform.xml', 'w')
-f.write(  platform.toprettyxml() )
+f.write( '<?xml version=\'1.0\'?>\n<!DOCTYPE platform SYSTEM "http://simgrid.gforge.inria.fr/simgrid.dtd">\n')
+tree.write(f)
+
+#         +ET.tostring(platform) )
+#f.write( doc.toxml() )
 f.close()
 
 
